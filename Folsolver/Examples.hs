35ae44527a34ab81a975dda41b60f0dba46dfa14
Marco Traeger <traeger@inf.fu-berlin.de>
2012-10-01 17:06:50 +0200

    added first example (simple axiom) to test the tptp parser. useage: 'ghci Folsolver/Examples.hs', 'axiom' will print the axiom readed from 'Data/Examples/axiom.tptp'.

---
+++ Folsolver/Examples.hs
@@ -0,0 +1,18 @@
+module Folsolver.Examples
+ ( readExample, readExampleUnsafe
+ , axiom
+ ) where
+
+import System.IO
+import System.IO.Unsafe
+import Codec.TPTP
+
+readExample :: String -> IO [TPTP_Input]
+readExample filename = do
+  content <- readFile filename
+  return $ parse content
+
+readExampleUnsafe :: String -> [TPTP_Input]
+readExampleUnsafe = unsafePerformIO . readExample
+
+axiom = readExampleUnsafe "Data/Examples/Axiom.tptp"
