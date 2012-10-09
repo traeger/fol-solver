d3d954afb8c344ab12947c4a6f4333cca80f512b
Max Wisniewski <max.wisniewski@fu-berlin.de>
2012-10-09 17:19:26 +0200

    Written examples.

--- Folsolver/Examples.hs
+++ Folsolver/Examples.hs
@@ -1,7 +1,4 @@
-module Folsolver.Examples
- ( readExample, readExampleUnsafe
- , axiom1, axiom2, axiom3, f, uninterfunc
- ) where
+module Folsolver.Examples where
 
 import System.IO
 import System.IO.Unsafe
@@ -20,3 +17,10 @@ axiom2 = readExampleUnsafe "Data/Examples/axiom2.tptp"
 axiom3 = readExampleUnsafe "Data/Examples/axiom3.tptp"
 uninterfunc = readExampleUnsafe "Data/Examples/uninterfunc.tptp"
 f = readExampleUnsafe "Data/Examples/f.tptp"
+quant1 = readExampleUnsafe "Data/Examples/quant_1.tptp"
+quant2 = readExampleUnsafe "Data/Examples/quant_2.tptp"
+quant3 = readExampleUnsafe "Data/Examples/quant_3.tptp"
+quant4 = readExampleUnsafe "Data/Examples/quant_4.tptp"
+quant5 = readExampleUnsafe "Data/Examples/quant_5.tptp"
+
+agatha = readExampleUnsafe "Data/Examples/agatha.tptp"
